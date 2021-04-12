11214608fb7438def4392e1dfd03ef251de7a7dc
root@debian10 <gitlab@logicmoo.org>
2021-04-12 06:54:29 -0700

    target

--- StartLogicmoo.sh
+++ StartLogicmoo.sh
@@ -8,12 +8,10 @@ if [[ $EUID -ne 0 ]]; then
 else
 
 ((
-(source ./INSTALL.sh)
+( source ./INSTALL.sh )
 sudo -u prologmud_server -- sh -c "${LOGICMOO_WS}/logicmoo_env.sh ; . ${LOGICMOO_WS}/packs_web/butterfly/bin/activate ; export LOGICMOO_WS=$LOGICMOO_WS ; cd ${LOGICMOO_GAMES} ; ./StartMUD.sh $*"
 ))
 stty sane
 
-
-)
 fi
 
