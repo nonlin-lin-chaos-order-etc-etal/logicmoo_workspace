248a56720b997d19c7eff6263d17bfe91788e0eb
Douglas R Miles <logicmoo@gmail.com>
2021-04-23 02:28:25 -0700

    Docker Fri 23 Apr 2021 02:28:22 AM PDT

--- packs_sys/logicmoo_nlu/ext/stanford/LogicmooStanford.sh
+++ packs_sys/logicmoo_nlu/ext/stanford/LogicmooStanford.sh
@@ -13,16 +13,17 @@ if [[ ! -f deps/stanford-corenlp-4.2.0-models.jar ]]; then
 )
 fi
 
-#set +x +e
 
 export OPTS="edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 4090 -preload -status_port 4091 -timeout 15000 $@"
 
 export OPTS="edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 4090 -preload -status_port 4091 -timeout 15000"
 
 # -annotators tokenize,quote,ssplit,pos,lemma,ner,truecase,parse,hcoref,relation
+echo cd `pwd`
 echo java -mx4g -cp "deps/*" $OPTS
 java -mx4g -cp "deps/*" $OPTS
 
+sleep 3
 echo "${BASH_SOURCE[0]} Exiting..."
 
 
