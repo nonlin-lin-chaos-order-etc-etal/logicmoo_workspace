88c98b4232614b1ce94b063de1230f1e7493d0bd
Douglas R Miles <logicmoo@gmail.com>
2021-04-24 10:51:52 -0700

    shallow submodule

--- web_install.sh
+++ web_install.sh
@@ -40,11 +40,6 @@ git status -s
 
 (source ./INSTALL.md)
 
-echo -e "\e[1;32m ":+
-        "If Docker is installed ":+
-        "Type:  ":+
-        "  ./runFromDocker.sh":+
-        "otherwise this might work:":+
-        "  source ./StartLogicmoo.sh \e[0m"
+echo -e "\e[1;32m If Docker is installed: ./runFromDocker.sh  (otherwise: source ./StartLogicmoo.sh)\e[0m"
 
 )
